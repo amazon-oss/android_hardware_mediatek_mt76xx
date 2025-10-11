34133b228f9f78a15816a90ee461a5875a6c2857
bengris32 <bengris32@protonmail.ch>
2025-10-11 22:30:19 +0100

    libbt-vendor: Clean-up Makefile
    - Remove support for multiple Bluetooth HALs.
    - Merge libbluetooth_mtk and libbt-vendor into a single shared library.
    
    Change-Id: I0d31f150f0c26430f70bcdb9ea9aa0dc9905afaf
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- libbt-vendor/Android.mk
+++ libbt-vendor/Android.mk
@@ -4,61 +4,30 @@ include $(CLEAR_VARS)
 
 BDROID_DIR := $(TOP_DIR)system/bt
 
+###########################################################################
+# MTK BT DRIVER FOR BLUEDROID
+###########################################################################
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
+  bt_drv.c \
   mtk.c \
-  radiomgr.c\
+  radiomgr.c \
   platform.c
 
 LOCAL_C_INCLUDES := \
-  $(BDROID_DIR)hci/include \
-  system/bt/hci/include
+    $(BDROID_DIR)hci/include \
+    system/bt/hci/include
 
+LOCAL_CFLAGS += -DMTK_VENDOR_OPCODE=FALSE
+LOCAL_CFLAGS += -DMTK_BPERF_ENABLE=FALSE
 
-LOCAL_CFLAGS += -DMTK_MT7662
-
-ifeq ($(TARGET_BUILD_VARIANT), eng)
-LOCAL_CFLAGS += -DBD_ADDR_AUTOGEN
-endif
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libbt-vendor
 
 LOCAL_SHARED_LIBRARIES := \
         libcutils \
         libutils \
         liblog
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libbluetooth_mtkbt
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
-
-###########################################################################
-# MTK BT DRIVER FOR BLUEDROID
-###########################################################################
-include $(CLEAR_VARS)
-$(info hello mtk)
-LOCAL_SRC_FILES := \
-  bt_drv.c \
-  FallthroughBTA.cpp
-
-LOCAL_C_INCLUDES := \
-  $(BDROID_DIR)/hci/include \
-  
-
-LOCAL_CFLAGS += -DMTK_VENDOR_OPCODE=FALSE
-LOCAL_CFLAGS += -DMTK_BPERF_ENABLE=FALSE
-
-LOCAL_MODULE_TAGS := optional
-ifeq ($(BOARD_HAVE_BLUETOOTH_MULTIBT),true)
-	LOCAL_MODULE := libbt-vendor_mtk
-else
-	LOCAL_MODULE := libbt-vendor
-endif
-
-LOCAL_SHARED_LIBRARIES := \
-        libcutils \
-        libutils \
-        liblog \
-        libbluetooth_mtkbt
-
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
